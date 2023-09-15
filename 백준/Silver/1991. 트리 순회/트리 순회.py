3de344432fe8b28385776caa23d7d82ad24123fc
ksb143 <71137594+ksb143@users.noreply.github.com>
2023-09-16 01:48:23 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,32 +1,46 @@
-def preorder(p):
-    if p != '.':
-        print(p, end='')
-        preorder(tree[p][0])
-        preorder(tree[p][1])
+def preorder(root, n):
+    if root != '.':
+        print(root, end='')
+        if ch1[n] != '.':
+            preorder(ch1[n], tree.index(ch1[n]))
+        if ch2[n] != '.':
+            preorder(ch2[n], tree.index(ch2[n]))
 
-def inorder(p):
-    if p != '.':
-        inorder(tree[p][0])
-        print(p, end='')
-        inorder(tree[p][1])
+def inorder(root, n):
+    if root != '.':
+        if ch1[n] != '.':
+            inorder(ch1[n], tree.index(ch1[n]))
+        print(root, end='')
+        if ch2[n] != '.':
+            inorder(ch2[n], tree.index(ch2[n]))
 
-def postorder(p):
-    if p != '.':
-        postorder(tree[p][0])
-        postorder(tree[p][1])
-        print(p, end='')
+def postorder(root, n):
+    if root != '.':
+        if ch1[n] != '.':
+            postorder(ch1[n], tree.index(ch1[n]))
+        if ch2[n] != '.':
+            postorder(ch2[n], tree.index(ch2[n]))
+        print(tree[n], end='')
 
 
 N = int(input())
 
-# 트리 딕셔너리
-tree = {}
+# 트리 내부에 있는 문자
+tree = [0] * (N+1)
+# 해당 트리의 자식 노드
+ch1 = [0] * (N+1)
+ch2 = [0] * (N+1)
 
-for _ in range(N):
-    p, ch1, ch2 = input().split()
-    tree[p] = [ch1, ch2]
-preorder('A')
+rm_set = {'.'}
+for i in range(1, N+1):
+    lst = input().split()
+    tree[i] = lst.pop(0)
+    if lst:
+        ch1[i] = lst.pop(0)
+    if lst:
+        ch2[i] = lst.pop(0)
+preorder('A', 1)
 print()
-inorder('A')
+inorder('A', 1)
 print()
-postorder('A')
+postorder('A', 1)
