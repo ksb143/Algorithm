0b04dc79b7891e81831fcffe411a52a3d8b240aa
ksb143 <71137594+ksb143@users.noreply.github.com>
2024-01-08 23:57:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.03 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,14 @@
+from collections import Counter
+
 def solution(participant, completion):
-    answer = ''
+    # 참여자와 완주자 목록의 각각의 요소 수를 세기
+    participant_counter = Counter(participant)
+    completion_counter = Counter(completion)
+
+    # 참여자 목록에서 완주자 목록을 뺀 차집합 구하기
+    answer_counter = participant_counter - completion_counter
+
+    # 남은 참여자 중 한 명이 완주하지 못한 사람
+    answer = list(answer_counter.keys())[0]
+
     return answer
