5d9b6d1379a746970f5a5e62dd608374e09a134a
ksb143 <71137594+ksb143@users.noreply.github.com>
2024-01-09 00:13:41 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,21 @@
-from collections import Counter
-
 def solution(participant, completion):
-    # 참여자와 완주자 목록의 각각의 요소 수를 세기
-    participant_counter = Counter(participant)
-    completion_counter = Counter(completion)
+    # 완주 선수를 체크하기 위한 딕셔너리 생성
+    check = dict()
+    # 참여자 선수 반복문
+    for par in participant:
+        # 딕셔너리에 해당 선수 이름이 있으면 인원 추가
+        if par in check:
+            check[par] += 1
+        # 딕셔너리에 해당 선수 이름이 없으면 해당 선수 이름 한명 추가
+        else:
+            check[par] = 1
 
-    # 참여자 목록에서 완주자 목록을 뺀 차집합 구하기
-    answer_counter = participant_counter - completion_counter
+    # 완주자 인원을 한명씩 제외
+    for com in completion:
+        check[com] -= 1
 
-    # 남은 참여자 중 한 명이 완주하지 못한 사람
-    answer = list(answer_counter.keys())[0]
-
+    # 모두 다 제외된 딕셔너리에 남은 인원 1인 있으면 그 인원이 완주 못한 사람
+    for key, value in check.items():
+        if value == 1:
+            answer = key
     return answer
