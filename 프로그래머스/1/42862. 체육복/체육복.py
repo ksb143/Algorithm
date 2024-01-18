3fdf38ee09d34e5abf098ced50ea797ed8331cc3
ksb143 <71137594+ksb143@users.noreply.github.com>
2024-01-18 21:30:39 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -4,19 +4,12 @@ def solution(n, lost, reserve):
     res_set = set(reserve)
     reserve = list(res_set - lost_set)
     lost = list(lost_set - res_set)
-    # 안들고 온 학생 빼기
-    n = n - len(lost)
     for res in reserve:
         # 작은 것부터 앞 뒤로 빌려줄 수 있는지 확인 후
         # 빌려줄 수 있다면 전체 학생수에서 +
         if res-1 in lost:
-            n += 1
             lost.remove(res - 1)
-        elif res in lost:
-            n += 1
-            lost.remove(res)
         elif res+1 in lost:
-            n += 1
             lost.remove(res + 1)
-    answer = n
+    answer = n - len(lost)
     return answer
