1e0125d9c95f97752647e91ae982ae453f05a17d
Subin Kim <71137594+ksb143@users.noreply.github.com>
2024-06-09 17:37:14 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,14 +1,15 @@
 def solution(citations):
-    # 인용된 수 확인
-    h = 0
-    while True:
-        cnt = 0
-        for citation in citations:
-            if citation >= h:
-                cnt += 1
-        if cnt >= h:
-            h += 1
+    # 인용 횟수 내림차순
+    citations.sort(reverse = True)
+    # 총 논문 개수
+    n = len(citations)
+    
+    h_index = 0
+    
+    for i in range(n):
+        # 인용 횟수가 순위보다 크거나 같으면 h_index 증가
+        if citations[i] >= i + 1:
+            h_index = i + 1
         else:
-            h -= 1
             break
-    return h
+    return h_index
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#fn1) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 09일 17:27:06
+2024년 06월 09일 17:37:12
 
 ### 문제 설명
 
