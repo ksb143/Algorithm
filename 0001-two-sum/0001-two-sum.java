f0209b3d999dd9b181d0e1cd3e89542cb210b3d2
Subin Kim <71137594+ksb143@users.noreply.github.com>
2024-12-11 17:53:12 +0900

    Update 0001.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
         int N = nums.length;
         int[] ans = new int[2];
 
+        // 이중 루프 한 번에 나오는 방법
+        loopOut:
         for (int i = 0; i < N; i++) {
-            int diff = target - nums[i];
-            // 만약 해쉬맵에 해당 숫자 있는지 확인
-            if (map.containsKey(diff)) {
-                ans[0] = map.get(diff);
-                ans[1] = i;
-                break;
+            for (int j = i + 1; j < N; j++) {
+                int temp = nums[i] + nums[j];
+                if (temp == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    break loopOut;
+                }
             }
-            // 해쉬맵에 없다면 현재 숫자를 key값, 인덱스를 map에 추가
-            map.put(nums[i], i);
         }
-        
+
         return ans;
     }
 }
