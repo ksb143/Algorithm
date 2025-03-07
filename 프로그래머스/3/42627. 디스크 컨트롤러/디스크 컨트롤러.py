be6c2ebd28c05eeded240568640f6bcd01f7da38
Subin Kim <71137594+ksb143@users.noreply.github.com>
2025-03-07 16:22:10 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,27 +1,31 @@
 import heapq
 
 def solution(jobs):
-    job_cnt = len(jobs) # job 개수
-    total_time = 0 # 총 대기 시간
-    start, end, i = -1, 0, 0 # 이전 작업, 현재 시간, 처리 작업수
-    heap = [] # 우선순위 큐
+    job_cnt = len(jobs)  # job 개수
+    total_time = 0  # 총 대기 시간 + 작업 시간
+    end, i = 0, 0  # 끝나는 시간, jobs 인덱스
+    start = -1  # 시작 시간 초기화
+    heap = []  # 우선 순위 큐
+
+    jobs.sort()  # 시작 시간 기준으로 정렬
     
-    jobs.sort() # 시작 시간 기준 정렬
-    
-    while i < job_cnt:
-        # 현재 시간 이전에 시작한 모든 작업 힙에 추가
-        for job in jobs:
-            if start < job[0] <= end:
-                heapq.heappush(heap, (job[1], job[0]))
-                
+    while i < job_cnt or heap:
+        # 현재 시간 이전에 시작한 모든 작업들을 힙에 추가
+        for j in range(i, job_cnt):
+            if start < jobs[j][0] <= end:
+                heapq.heappush(heap, (jobs[j][1], jobs[j][0]))
+                i += 1
+            else:
+                break
+
+        # 힙에서 작업을 하나 꺼내 처리
         if heap:
             current_job = heapq.heappop(heap)
             start = end
             end += current_job[0]
             total_time += end - current_job[1]
-            i += 1
-        else:
-            end += 1
-            
+        else:  # 현재 처리할 작업이 없다면, 다음 작업의 시작 시간으로 점프
+            end = jobs[i][0]
+
     answer = total_time // job_cnt
     return answer
