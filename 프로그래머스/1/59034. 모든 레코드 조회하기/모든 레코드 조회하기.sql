3cb919ad09844d49623f4479f84b36b58ccc6093
Subin Kim <71137594+ksb143@users.noreply.github.com>
2025-05-02 21:52:51 +0900

    [level 1] Title: 모든 레코드 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
+++ 프로그래머스/1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
@@ -1,10 +1,4 @@
 -- 코드를 입력하세요
--- ANIMAL_ID: 이이디
--- ANIMAL_TYPE: 생물 종
--- DATETIME: 보호 시작일
--- INTAKE_CONDITION: 보호 시작 시 상태
--- NAME: 이름
--- SEX_UPON_INTAKE: 성별 및 중성화 여부
 SELECT *
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
