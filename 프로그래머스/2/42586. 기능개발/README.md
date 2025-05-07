e2ab96717154f3f440025d1e317e297d9db7a910
Subin Kim <71137594+ksb143@users.noreply.github.com>
2025-05-07 15:26:53 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.17 ms
+메모리: 72.8 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 19일 00:21:34
+2025년 05월 07일 15:26:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,56 +1,35 @@
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Arrays;
-import java.util.ArrayList;
-
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 빈 큐 만들기
-        Queue<Integer> p = new LinkedList<>();
-        Queue<Integer> s = new LinkedList<>();
-
-        // 큐에 num 삽입
-        // add는 큐가 꽉 찬 경우 IllegalStateException 반환
-        // offer는 큐가 꽉 찬 경우 false 반환
-        int len = progresses.length;
-        for (int i = 0; i < len; i++) {
-            p.offer(progresses[i]);
-            s.offer(speeds[i]);
-
-        }
-        ArrayList<Integer> answer = new ArrayList<>();
-
-        // 큐에 무엇이 남아 있다면 반복문 계속
-        while (!p.isEmpty()) {
-            int cnt = p.size();
-            // 진행도 올리기
-            for (int i = 0; i < cnt; i++) {
-                // poll은 큐의 첫 번째 값 제거 및 반환
-                int progress = p.poll();
-                int speed = s.poll();
-                progress += speed;
-                p.offer(progress);
-                s.offer(speed);
+        int[] minimumDate = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            int num = 100 - progresses[i];
+            if (num % speeds[i] == 0) {
+                minimumDate[i] = num / speeds[i];
+            } else {
+                minimumDate[i] = num / speeds[i] + 1;
             }
-            // 큐 첫 기능이 100% 이상일 때
-            if (p.peek() >= 100) {
-                int comp = 0;
-                // 큐가 있고 100%가 넘으면 반복문 계속
-                while (!p.isEmpty() && p.peek() >= 100) {
-                    p.poll();
-                    s.poll();
-                    comp += 1;
+        }
+        List<Integer> lst = new ArrayList<>();
+        int idx = 1;
+        while (idx <= progresses.length) {
+            int preDate = minimumDate[idx - 1];
+            int cnt = 1;
+            for (; idx < progresses.length; idx++) {
+                if (preDate >= minimumDate[idx]) {
+                    cnt++;
+                } else {
+                    break;
                 }
-                // 100%가 안 넘으면 끝내기
-                answer.add(comp);
             }
+            lst.add(cnt);
+            idx++;
         }
-        // 배열로 변환
-        int[] result = new int[answer.size()];
-        for (int i = 0; i < answer.size(); i++) {
-            result[i] = answer.get(i);
+        int[] answer = new int[lst.size()];
+        for (int i = 0; i < lst.size(); i++) {
+            answer[i] = lst.get(i);
         }
-        return result;
+        return answer;
     }
 }
