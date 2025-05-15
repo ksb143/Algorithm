a7554cc7295029c40b4202fb6951b85d72fa177b
Subin Kim <71137594+ksb143@users.noreply.github.com>
2025-05-15 18:13:25 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 652 ms, Memory: 94464 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -9,19 +9,26 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int q = Integer.parseInt(st.nextToken());
 
+        int[][] counts = new int[26][S.length() + 1];
+
+        for (int i = 0; i < S.length(); i++) {
+            for (int c = 0; c < 26; c++) {
+                counts[c][i + 1] = counts[c][i];  // 이전까지 누적 복사
+            }
+            counts[S.charAt(i) - 'a'][i + 1]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
             char alpha = st.nextToken().charAt(0);
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cnt = 0;
-            for (int j = start; j < end + 1; j++) {
-                if (alpha == S.charAt(j)) {
-                    cnt++;
-                }
-            }
-            System.out.println(cnt);
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            sb.append(counts[alpha - 'a'][r + 1] - counts[alpha - 'a'][l]).append('\n');
         }
+
+        System.out.print(sb);
 
 
     }
