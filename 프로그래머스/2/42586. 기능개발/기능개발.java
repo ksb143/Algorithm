f6ae0a1604cf52d840971fe220214e00c3fc12a6
Subin Kim <71137594+ksb143@users.noreply.github.com>
2025-07-17 21:40:23 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,34 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] minimumDate = new int[progresses.length];
+        int[][] countChecks = new int[progresses.length][2];
         for (int i = 0; i < progresses.length; i++) {
-            int num = 100 - progresses[i];
-            if (num % speeds[i] == 0) {
-                minimumDate[i] = num / speeds[i];
+            int endCnt;
+            if ((100 - progresses[i]) % speeds[i] == 0) {
+                endCnt = (100 - progresses[i]) / speeds[i];
             } else {
-                minimumDate[i] = num / speeds[i] + 1;
+                endCnt = (100 - progresses[i]) / speeds[i] + 1;
+            }
+            countChecks[i][0] = i;  // 순서
+            countChecks[i][1] = endCnt;  // 순서
+        }
+
+        List<Integer> list = new ArrayList<>();
+        int minDay = countChecks[0][1];
+        int cnt = 0;
+        for (int[] cntCheck : countChecks) {
+            int idx = cntCheck[0];
+            int day = cntCheck[1];
+            if (day <= minDay) {
+                cnt++;  // 카운트 늘리기
+            } else {
+                list.add(cnt);  // cnt 삽입
+                cnt = 1;  // 카운트 초기화
+                minDay = day;  // 최소 기간 교체
             }
         }
-        List<Integer> lst = new ArrayList<>();
-        int idx = 1;
-        while (idx <= progresses.length) {
-            int preDate = minimumDate[idx - 1];
-            int cnt = 1;
-            for (; idx < progresses.length; idx++) {
-                if (preDate >= minimumDate[idx]) {
-                    cnt++;
-                } else {
-                    break;
-                }
-            }
-            lst.add(cnt);
-            idx++;
+        list.add(cnt);  // 마지막 남은 카운트 넣기
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
-        int[] answer = new int[lst.size()];
-        for (int i = 0; i < lst.size(); i++) {
-            answer[i] = lst.get(i);
-        }
+        
         return answer;
     }
 }
