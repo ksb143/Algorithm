7e5353d7dc8ec6e4bb5837aa0ebb43959b49d3ca
Subin Kim <71137594+ksb143@users.noreply.github.com>
2025-07-24 16:05:52 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] stairs = new int[N];
+        for (int i = 0; i < N; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[N];
+        dp[0] = stairs[0];
+
+        if (N > 1) {
+            dp[1] = stairs[0] + stairs[1];
+        }
+
+        if (N > 2) {
+            dp[2] = Math.max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
+        }
+
+        for (int i = 3; i < N; i++) {
+            dp[i] = Math.max(dp[i - 2] + stairs[i],
+                            dp[i - 3] + stairs[i - 1] + stairs[i]);
+        }
+
+        System.out.println(dp[N - 1]);
+    }
+}
