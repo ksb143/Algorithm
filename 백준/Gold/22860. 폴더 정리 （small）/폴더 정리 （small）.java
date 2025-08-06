9ed61306cb29d27bb0fdfc4d71ef2d3a8acb1ecb
Subin Kim <71137594+ksb143@users.noreply.github.com>
2025-08-06 16:30:58 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 980 ms, Memory: 131180 KB -BaekjoonHub

---
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class File {
+        String fileName;
+        boolean isFile;
+
+        File(String fileName, boolean isFile) {
+            this.fileName = fileName;
+            this.isFile = isFile;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof File)) return false;
+            File file = (File) o;
+            return isFile == file.isFile && Objects.equals(fileName, file.fileName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(fileName, isFile);
+        }
+    }
+
+    private static int[] bfs(HashMap<File, List<File>> map, String folder) {
+        HashSet<String> set = new HashSet<>();
+        int cnt = 0;
+        File sf = new File(folder, false);
+        Queue<File> q = new LinkedList<>();
+        q.offer(sf);
+        while (!q.isEmpty()) {
+            File cf = q.poll();
+            if (!map.containsKey(cf)) {
+                continue;
+            }
+            for (File f : map.get(cf)) {
+                if (f.isFile) {  // 파일이면 증가
+                    set.add(f.fileName);
+                    cnt++;
+                } else {  // 폴더면 큐에 추가
+                    q.add(f);
+                }
+            }
+        }
+        return new int[]{set.size(), cnt};
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<File, List<File>> map = new HashMap<>();
+        for (int i = 0; i < N + M; i++) {
+            st = new StringTokenizer(br.readLine());
+            String P = st.nextToken();  // 상위 폴더
+            File pf = new File(P, false);
+            String F = st.nextToken();  // 폴더 혹은 파일 이름
+            int C = Integer.parseInt(st.nextToken());  // 폴더면 1, 파일이면 0
+            File cf;
+            if (C == 1) {
+                cf = new File(F, false);
+            } else {
+                cf = new File(F, true);
+            }
+            map.computeIfAbsent(pf, k -> new ArrayList<>()).add(cf);
+        }
+
+
+        st = new StringTokenizer(br.readLine());
+        int Q = Integer.parseInt(st.nextToken());  // 쿼리 수
+        for (int i = 0; i < Q; i++) {
+            String[] query = br.readLine().split("/");
+            int[] ans = bfs(map, query[query.length - 1]);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
